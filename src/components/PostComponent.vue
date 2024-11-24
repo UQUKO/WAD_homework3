426a8b1ccc315f44f73f8b89431b5c3a83b62462
im-byte <rainervana1@gmail.com>
2024-11-24 10:55:19 +0200

    Add a working like button to the posts.

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -10,22 +10,23 @@
     <p v-html="formattedText"></p>
 
     <!-- Post Image -->
-    <img v-if="post.postImage" :src="post.postImage" alt="Couldn't load image" width="300" />
+    <img v-if="post.postImage" :src="post.postImage" alt="Couldn't load image" width="300" class="postImg"/>
 
     <!-- Like Button -->
-    <div class="like">
-      <img src="@/assets/youtube-like-button-png-11.png" alt="Like" width="25" height="25" />
-    </div>
+    <div class="likes-container">
+      <div class="like" @click="toggleLike">
+        <img :src="likeButtonImage" alt="Like" width="25" height="25" />
+      </div>
 
-    <!-- Post Likes -->
-    <div class="post-likes">
-      <p>Likes: {{ post.postLikes }}</p>
+      <!-- Post Likes -->
+      <div class="post-likes">
+        <p>{{ likeAmount }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "PostComponent",
   props: {
@@ -45,19 +46,47 @@ export default {
     formattedText() {
       return this.post.postText.replace(/\n/g, "<br>");
     },
+    likeButtonImage() {
+      return this.isLiked
+        ? require('@/assets/youtube-like-button-png-12.png')
+        : require('@/assets/youtube-like-button-png-11.png');
+    }
+  },
+  data() {
+    return {
+      isLiked: false,
+      likeAmount: this.post.postLikes,
+    };
+  },
+  methods: {
+    toggleLike() {
+      if (this.isLiked) {
+        this.likeAmount -= 1;
+      } else {
+        this.likeAmount += 1;
+      }
+      this.isLiked = !this.isLiked;
+    },
   },
 };
 </script>
 
 <style scoped>
-.post{
-    border: 1px;
-    padding: 20px;
-    background-color: rgb(173, 225, 176);
-    border-radius: 15px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    width: 50%;
-    align-content: center;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+
+
+.post {
+  border: 1px;
+  padding: 20px;
+  background-color: rgb(173, 225, 176);
+  border-radius: 15px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 50%;
+  align-content: center;
+}
+
+.postImg {
+  margin: 0.5em 0em 1em 0em;
 }
 
 header {
@@ -73,5 +102,16 @@ header img {
 
 .like img {
   cursor: pointer;
+}
+
+.post-likes p {
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+}
+
+.likes-container {
+  display: flex;
+  align-items: center;
+  gap: 0.8em;
 }
 </style>
